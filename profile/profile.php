5337da5d100fc7b67d2c16ec4afe53d0725fd89d
meriamc <cherifmeriam004@gmail.com>
2024-04-21 09:34:22 +0100

    aa

--- profile/profile.php
+++ profile/profile.php
@@ -2,6 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require('connexion.php');
 
+    $username = $_POST['username'] ?? '';
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $bio = $_POST['bio'] ?? '';
+    $ville = $_POST['ville'] ?? '';
+    $adresse = $_POST['adresse'] ?? '';
+    $codepostal = $_POST['codepostal'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+
+    $sql = "INSERT INTO users (username, name, email, bio, ville, adresse, codepostal, phone)
+    VALUES ('$username', '$name', '$email', '$bio', '$ville', '$adresse', '$codepostal', '$phone')";
+
 
 }
 
